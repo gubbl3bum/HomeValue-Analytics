390d9abca0e2f7e68e5afeb491a21838a43d844c
gubbl3bum <gbien13k@gmail.com>
2025-05-21 06:40:14 +0200

    Add missing import, add hints for target encoding

--- src/data_encoding.py
+++ src/data_encoding.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def one_hot_encode(df, column):
     """
     One-Hot Encoding dla wybranej kolumny.
@@ -10,9 +12,13 @@ def one_hot_encode(df, column):
 def target_encode(df, column, target):
     """
     Target Encoding dla wybranej kolumny.
+
     :param df: DataFrame z danymi
-    :param column: Nazwa kolumny
-    :param target: Nazwa kolumny docelowej
+    :param column: Nazwa kolumny do zakodowania (symbolicznej/kategorycznej)
+    :param target: Nazwa kolumny docelowej (zwykle liczbowej, np. cena, wartość, wynik)
+                   Kolumna docelowa to ta, względem której liczona jest średnia dla każdej kategorii.
+                   Przykład: jeśli chcesz zakodować kolumnę 'miasto' względem średniej ceny domu,
+                   ustaw 'miasto' jako column, a 'cena' jako target.
     :return: DataFrame z zakodowaną kolumną
     """
     means = df.groupby(column)[target].mean()
